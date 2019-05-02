7be7c64dd1f98e1ceee439a6b7a49f4d48bed59d
Sofia Sousa <csofiamsousa@gmail.com>
2019-05-02 14:24:11 +0100

    TP31067 feat: Remove unnecessary style for editor and fix external-assets block description

--- src/external-assets/index.js
+++ src/external-assets/index.js
@@ -7,12 +7,11 @@ import { element, i18n } from 'wp';
 const { Fragment } = element;
 const { __ } = i18n;
 
-
 export const name = 'external-assets';
 
 export const settings = {
   title: __('Styles And Scripts'),
-  description: __('Import the css styles and js scripts for Nordic Innovation'),
+  description: __('Import the css styles and js scripts for IBM'),
   icon: 'editor-code',
 
   attributes: {},
@@ -27,11 +26,8 @@ export const settings = {
     return (
       <Fragment>
         <div style={ style }>Styles and Scripts</div>
-        <link rel="stylesheet" href="//1.cms.s81c.com/sites/default/files/css/css_2mWlM7uzLsX26o3-VPopAEsrrqZCpfeSCocID4Z5OGs.css?pqpdq2" media="all" />
         <link rel="stylesheet" href="//1.www.s81c.com/common/v18/css/www.css" media="all" />
-        <link rel="stylesheet" href="//1.www.s81c.com/common/v18/css/tables.css" media="all" />
         <link rel="stylesheet" href="//1.www.s81c.com/common/v18/css/grid-fluid.css" media="all" />
-        <link rel="stylesheet" href="//1.cms.s81c.com/sites/default/files/css/css_EneiLCH2C7OvkDxQNoz70doseF_bb-0-hJL5DCnDePI.css?pqpdq2" media="all" />
       </Fragment>
     );
   },
